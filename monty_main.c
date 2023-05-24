3aa6dd69c4527db1204cca6d732091363f7ddb23
uelkariuki <cratzuel@gmail.com>
2023-05-24 22:50:57 +0000

    task 1

--- monty_main.c
+++ monty_main.c
@@ -134,7 +134,7 @@ void pint(stack_t **stack)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L %d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
