7ac5c328ecc0c81158e10fc3ab35a1987bb9a263
uelkariuki <cratzuel@gmail.com>
2023-05-25 19:53:40 +0000

    task 9

--- monty.h
+++ monty.h
@@ -51,6 +51,7 @@ void add(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void div_func(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
 
 
 
--- monty_main.c
+++ monty_main.c
@@ -67,6 +67,8 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 				div_func(&stack, line_number);
 			else if (strcmp(opcode, "mul") == 0)
 				mul(&stack, line_number);
+			else if (strcmp(opcode, "mod") == 0)
+				mod(&stack, line_number);
 			else
 			{
 				fprintf(stderr, "L%d: unknown instruction %s\n",
