27ab7dd0a52cfdd8f2b17ddbbfc44ca8a8831878
uelkariuki <cratzuel@gmail.com>
2023-05-25 18:08:10 +0000

    task 4

--- add.c
+++ add.c
@@ -12,7 +12,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 		if (top_of_stck == NULL || top_of_stck->next == NULL)
 		{
-			fprintf(stderr, "L%d: can't add, stack too short", line_number);
+			fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 			exit(EXIT_FAILURE);
 		}
 
