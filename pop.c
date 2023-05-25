f00f2a4c7118e4800cdfdfc8928204a99b629795
uelkariuki <cratzuel@gmail.com>
2023-05-25 18:23:44 +0000

    task 4v2

--- pop.c
+++ pop.c
@@ -9,20 +9,27 @@
 void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top_node_stack;
+	char *args;
 
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	args = strtok(NULL, " \t\n");
+	if (args != NULL)
+	{
+		fprintf(stderr, "L%d: usage: pop\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	top_node_stack = *stack;
 
-	if ( top_node_stack-> next != NULL)
+	if (top_node_stack->next != NULL)
 	{
 		/* 7 <-> 6 <-> 5 <-> 3 */
 		/*top_node (7) next is (6), prev of (6) is (7) which*/
 		/* is set to NULL thus removing it*/
-		top_node_stack-> next->prev = NULL;
+		top_node_stack->next->prev = NULL;
 	}
 	*stack = top_node_stack->next;
 }
