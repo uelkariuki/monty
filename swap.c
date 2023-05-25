1b8d953528f5079b633648851e0d81d98a49d7c4
uelkariuki <cratzuel@gmail.com>
2023-05-25 17:08:26 +0000

    task 3v1

--- swap.c
+++ swap.c
@@ -7,7 +7,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (top_of_stack == NULL || top_of_stack->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
