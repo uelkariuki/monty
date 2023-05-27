70ead969269f21c2027d3bb087dfcdac0c4babfd
uelkariuki <cratzuel@gmail.com>
2023-05-27 15:56:09 +0000

    task 0 Fix v2

--- monty_main.c
+++ monty_main.c
@@ -107,8 +107,8 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 		}
 		line_number++;
 	}
-	free_the_stack(&stack);
 	fclose(file_pointer);
+	free_the_stack(&stack);
 	return (0);
 }
 /**
